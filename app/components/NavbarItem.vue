6abcb6fc1c078eca43dca9f173108a3dee53f86f
Aykss <castorberkeleyaieca@gmail.com>
2025-12-22 14:49:32 +0800

    feat: ui design fix for pc and mobile

--- app/components/NavbarItem.vue
+++ app/components/NavbarItem.vue
@@ -2,7 +2,8 @@
 const props = defineProps<{
   title: string,
   img?: string,
-  route: string
+  route: string,
+  custom_class?: string
 }>()
 </script>
 
@@ -11,7 +12,7 @@ const props = defineProps<{
     <img :src="props.img" alt="Flowbite Logo" class="block p-2 rounded-sm hover:bg-mugal-green" />
   </template>
   <template v-else>
-    <NuxtLink :to="props.route" class="block py-2 px-3 text-white rounded-sm hover:bg-mugal-green">
+    <NuxtLink :to="props.route" :class="custom_class" class="md:text-lg block py-2 px-3 text-white rounded-sm hover:bg-mugal-green">
       {{ props.title }}
     </NuxtLink>
   </template>
