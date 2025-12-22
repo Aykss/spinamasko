f47a847ca35d273e2653d473f86e5d5a408a8e93
Aykss <castorberkeleyaieca@gmail.com>
2025-12-22 15:22:58 +0800

    feat: fix the login page

--- app/app.vue
+++ app/app.vue
@@ -3,10 +3,10 @@
 
 <template>
   <div class="w-full h-screen bg-yellow-50 flex flex-col items-center overflow-auto">
-    <div class="w-full xl:flex xl:justify-center bg-red-600 sticky top-0 z-40 border-b-4 border-yellow-400">
+    <div class="w-full xl:flex xl:justify-center bg-red-600 border-b-4 border-yellow-400">
         <NavBar />
     </div>
-     <div class="w-full flex flex-col justify-center items-center max-w-384 z-10 min-h-11/12 gap-10">
+     <div class="w-full flex flex-col justify-center items-center max-w-384 z-10 min-h-fit gap-10 pb-10">
         <NuxtPage/>
     </div>
     <!-- <div class="w-full xl:flex xl:justify-center bg-red-600 border-t-4 border-yellow-400 p-5">
