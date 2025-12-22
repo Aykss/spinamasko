5c432615ffa45e9234b43ba07da5600ec23784dd
Aykss <castorberkeleyaieca@gmail.com>
2025-12-22 14:19:45 +0800

    chore: component name fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,13 +10,13 @@ export default withNuxt(
   {
     files: ["components/**/*.vue"], // ✅ only check Vue files in components/
     plugins: { prettier },
+    rules: {
+      // Disable the multi-word component name rule
+      'vue/multi-word-component-names': 'off',
+    },
   },
   {
     files: ["test/**/*.ts"], // ✅ only check TS files in server/
     plugins: { prettier },
-  },
-  rules: {
-    // Disable the multi-word component name rule
-    'vue/multi-word-component-names': 'off',
   },
 )
